1502dd14558c484ca50a86498f606bc70b5d59cc
JustSock <sockincabbadge@gmail.com>
2024-11-29 23:34:40 +0500

    Little Adjustments
    Little Adjustments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import MainBlock from './Components/PizzaCard';
 import img1 from './images/img1.png';
+import img2 from './images/img2.png';
 
 function App(){
     return(
-        <div>
-            <div className="main-block-cover">
-                <MainBlock title="Пепперони" img={img1} text='Ничего лишнего! Томатный соус, колбаски Пепперони и Моцарелла'/>
-            </div>
+        <div className="main-block-cover">
+            <MainBlock title="Пепперони" img={img1} img2={img2} text='Ничего лишнего! Томатный соус, колбаски Пепперони и Моцарелла'/>
         </div>
     );
 }
--- src/Components/PizzaCard.jsx
+++ src/Components/PizzaCard.jsx
@@ -4,15 +4,34 @@ function PizzaCard(props){
     const [count, setCount] = useState(0);
     return(
         <div className="pizza-card">
-            <div className="main-img"><img src={props.img}/></div>
-            <h4 className="main-block-header">{props.title}</h4>            
-            <p className="main-block-text">{props.text}</p>
-            <p className="main-dropdown">Диаметр</p>
-            <p className="main-price">от 550 ₽</p>
-            <button className="main-button" onClick={() => setCount(count - 1)}>-</button>
-            <p className="main-dropdown">{count}</p>
-            <button className="main-button" onClick={() => setCount(count + 1)}>+</button>
-            <button className="main-buy">Купить</button>
+            <div className="main-img"><img src={props.img} /></div>
+            <div class="text-block">
+                <h3>{props.title}</h3>
+                <p>{props.text}</p>
+                <select>
+                    <option>Диаметр</option>
+                    <option>10</option>
+                    <option>15</option>
+                    <option>25</option>
+                </select>
+
+                <h4>от 550 ₽</h4>
+               
+                <div class="btn-group">
+                <button className="main-button" onClick={function(){if (count>0) {setCount(count - 1)}}}>-</button>
+                    <button className="main-dropdown">{count}</button>
+                    <button className="main-button" onClick={() => setCount(count + 1)}>+</button>
+                </div>
+                <div class="btn-group">
+                    <button className="main-buy">
+                        <img src={props.img2} />
+                    </button>
+                </div>
+                
+               
+                
+            </div>
+            
         </div>
     );
 }
